57662f4e6cb142b03966f3334a51d6316d3ad043
adi-uchiha <adi.cs.night@gmail.com>
2024-04-22 13:25:54 +0530

    Add department vist image

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,50 @@
+import Banners from "@/components/Banners";
 import LinkBar from "@/components/LinkBar";
 import MemberObject from "@/components/MemberData";
 import PersonCard from "@/components/PersonCard";
 import Title from "@/components/Title";
+import Image from "next/image";
 
 // 
 export default function Home() {
   return (
     <div className="flex flex-col lg:items-center">
+      {/* <Banners /> */}
+      <div>
+        <div className="title-div">
+          <span className={`pl-5 flex w-full justify-center lg:text-3xl font-bold text-white mt-8 p-2 sm:text-lg`}>Pillars of the AIDS Department</span>
+        </div>
+        <img className="md:w-full lg:w-[75vw]" src="/banner-images/pillars.jpeg" alt="Aideas Logo" />
+      </div>
+
+      <div>
+        <div className="title-div">
+          <span className={`pl-5 flex w-full justify-center lg:text-3xl font-bold text-white mt-8 p-2 sm:text-lg`}>Smart City Pune Vist</span>
+        </div>
+        <img className="md:w-full lg:w-[75vw]" src="/banner-images/smart-city.jpeg" alt="Aideas Logo" />
+      </div>
+
+      <div>
+        <div className="title-div">
+          <span className={`pl-5 flex w-full justify-center lg:text-3xl font-bold text-white mt-8 p-2 sm:text-lg`}>PVG Sanstha Visit</span>
+        </div>
+        <img className="md:w-full lg:w-[75vw]" src="/banner-images/pvg-sanstha.jpeg" alt="Aideas Logo" />
+      </div>
+
+      <div>
+        <div className="title-div">
+          <span className={`pl-5 flex w-full justify-center lg:text-3xl font-bold text-white mt-8 p-2 sm:text-lg`}>World Food day celebration</span>
+        </div>
+        <img className="md:w-full lg:w-[75vw]" src="/banner-images/food-day.jpeg" alt="Aideas Logo" />
+      </div>
+
+      <div>
+        <div className="title-div">
+          <span className={`pl-5 flex w-full justify-center lg:text-3xl font-bold text-white mt-8 p-2 sm:text-lg`}>Donation to Salvation Army</span>
+        </div>
+        <img className="md:w-full lg:w-[75vw]" src="/banner-images/salvation-army.jpeg" alt="Aideas Logo" />
+      </div>
+
       <div className="space-y-6">
         <LinkBar link=" https://youtu.be/biUc9BVwhk8" type="youtube" />
         <LinkBar link="https://www.linkedin.com/company/aideas-pvg/" type="linkedin" />
@@ -28,7 +66,7 @@ export default function Home() {
       <PersonCard user={MemberObject[10]} />
       <Title title={"Web Team"} />
       <PersonCard user={MemberObject[14]} />
-      <Title title={"Event Team"}  />
+      <Title title={"Event Team"} />
       <PersonCard user={MemberObject[11]} />
       <Title title={"Editorial and Notification Team"} />
       <PersonCard user={MemberObject[12]} />
